4c90494992295c53ea3c626e072b94a7dbe9781c
Yuvadi29 <adityatrivedi612@gmail.com>
2023-06-29 14:58:43 +0530

    Social Media Done

--- src/app/page.js
+++ src/app/page.js
@@ -4,15 +4,19 @@ import AboutMe from './components/AboutMe';
 import Techstack from './components/Techstack';
 import ProjectSection from './components/ProjectSection';
 import About from './components/ProfileSection/About';
+import Social from './components/ProfileSection/Social';
 
 export default function Home() {
   return (
     // main section
     <section className="font-firaCode bg-[#22272e] h-auto w-auto flex justify-center items-center">
       {/* // left section */}
-      <section className="h-[250vh] w-[25%]">
+      <section className="h-[250vh] w-[25%] flex flex-col justify-center items-center">
         <About />
         <div className="my-2 w-[300px] border-2 border-[#768390]"></div>
+        <Social />
+        <div className="my-2 w-[300px] border-2 border-[#768390]"></div>
+
       </section>
       {/* right section  */}
       <section className="h-auto w-[75%] border-l-2 border-[#768390] flex flex-col justify-start items-center">
